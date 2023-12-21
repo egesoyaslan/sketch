919d0392099cf7fdce15748310657a6557bd9bab
egesoyaslan <ege.soyaslan@yahoo.com>
2023-12-21 19:21:48 +0300

    Edit index.html and script.js
    Added a text field that sets the grid to the input.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function makeGrid(n) {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    
+
     let sqr = n * n;
     while (sqr > 0) {
         const pixel = document.createElement(`div`);
@@ -16,7 +16,23 @@ function makeGrid(n) {
     }
 }
 
-makeGrid(16);
+function getValue() {
+    const field = document.querySelector(`#user`);
+    const inputValue = field.value;
+    return inputValue;
+}
+
+const button = document.querySelector(`#send`);
+button.addEventListener(`click`, () => {
+    const x = getValue();
+    if (x <= 100 && x > 0) {
+        makeGrid(x);
+    } else if (x > 100 || x <= 0) {
+        alert(`Plese enter a value between 1 and 100.`);
+    } else {
+        alert(`Please only enter number values.`);
+    }
+});
 
 container.addEventListener('mouseover', (e) => {
     const target = e.target;
